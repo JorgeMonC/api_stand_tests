e470b4723b8b11dae8a6d39cb60755caf9a13f65
Jorge Monroy <jorgemonroycorrea@gmail.com>
2024-10-24 15:19:24 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-388798f3-9b12-4c44-9ab4-2804e2c91972.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
